20461b08e003289448af9b51b366f26a58536918
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-04-23 13:32:21 -0300

    navbar and headline

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,15 +4,19 @@ const About = () => {
 	return (
 		<section id="about">
 			<div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-				<div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+				<div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center w-80 mx-8">
 					<h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-						Hi, I'm Reed.
-						<br className="hidden lg:inline-block" />I love to build amazing apps.
+						Hi, I'm Lucas.
+						<br className="hidden lg:inline-block" />I Build Amazing Websites and Web Apps
 					</h1>
 					<p className="mb-8 leading-relaxed">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui laborum quasi, incidunt dolore
-						iste nostrum cupiditate voluptas? Laborum, voluptas natus?
+						I'm a web developer with a passion for building beautiful and functional websites. Whether
+						you're looking for a skilled developer to join your team or to bring your project to life.
 					</p>
+					<p className="mb-8 leading-relaxed">
+						Check out some of my work in the <b>Projects</b> section.
+					</p>
+
 					<div className="flex justify-center">
 						<a
 							href="#contact"
@@ -27,9 +31,6 @@ const About = () => {
 							See My Past Work
 						</a>
 					</div>
-				</div>
-				<div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-					<img className="object-cover object-center rounded" alt="hero" src="../../coding.svg" />
 				</div>
 			</div>
 		</section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,31 @@
 import React from 'react'
-import { BsArrowRight } from 'react-icons/bs'
 
 const Navbar = () => {
 	return (
-		<header className="bg-gray-800 md:sticky top-0 z-10">
-			<div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-				<a className="title-font font-medium text-white mb-4 md:mb-0">
-					<a href="#about" className="ml-3 text-xl">
-						Lucas Coppola
-					</a>
+		<header className="bg-gray-800 md:sticky top-0 z-10 px-16">
+			<div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
+				<a href="#about">
+					<img
+						src="../../public/LC.png"
+						alt="logo"
+						className="w-12 h-12 rounded-full my-1 border-2 border-white-500"
+					/>
 				</a>
-				<nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-					<a href="#projects" className="mr-5 hover:text-white">
-						About Me
+
+				<nav className="flex flex-wrap items-center justify-end md:ml-auto">
+					<a href="#projects" className="mx-5 text-lg hover:text-white">
+						Home
 					</a>
-					<a href="#skills" className="mr-5 hover:text-white">
+					<a href="#about" className="mx-5 text-lg hover:text-white">
+						About
+					</a>
+					<a href="#skills" className="mx-5 text-lg hover:text-white">
 						Projects
 					</a>
+					<a href="#contact" className="ml-5 text-lg hover:text-white">
+						Contact
+					</a>
 				</nav>
-				<a
-					href="#contact"
-					className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
-				>
-					Contact Me
-					<BsArrowRight className="w-4 h-4 ml-1" />
-				</a>
 			</div>
 		</header>
 	)
