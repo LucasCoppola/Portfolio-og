eb7aaea3df4fece0ea6254821435bb68105198fa
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-04-28 10:37:02 -0300

    about section responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,12 +3,14 @@ import { skillsData } from '../data'
 
 const About = () => {
 	return (
-		<section id="about" className="p-16">
+		<section id="about" className="md:px-10 lg:p-16">
 			<div className="container mx-auto flex flex-col  px-4">
-				<h1 className="mb-10 text-left text-3xl font-bold sm:text-4xl">About Me</h1>
+				<h1 className="mb-10 text-center text-3xl font-bold sm:text-4xl md:text-center lg:text-left">
+					About Me
+				</h1>
 				<div className="flex w-full flex-col md:flex-row md:justify-between">
 					<div className="mb-10 md:mb-0 md:w-1/2 md:pr-16">
-						<p className="text-center text-base leading-relaxed  dark:text-gray-400 md:text-left ">
+						<p className="text-center text-base leading-relaxed  dark:text-gray-400 md:text-left">
 							Hi! My name is Lucas and I'm a web developer based in Argentina. With a passion for web
 							development, I specialize in JavaScript, React, and Express to create awesome web
 							applications.
@@ -28,17 +30,17 @@ const About = () => {
 						</p>
 					</div>
 					<div className="md:w-1/2">
-						<h5 className="mb-6 text-center font-bold md:text-left md:text-2xl">Skills & Technologies</h5>
-						<div className="flex space-x-4 ">
+						<h5 className="mb-6 text-center font-bold md:text-2xl">Skills & Technologies</h5>
+						<div className="flex justify-around space-x-4	">
 							{skillsData.map((item) => {
 								return (
 									<div className="w-1/3">
 										<div className="mb-3 flex items-center rounded-lg bg-gray-200 px-4 py-2 dark:bg-gray-700">
-											<h2 className="text-base font-medium text-gray-900 dark:text-white">
+											<h2 className="text-base font-medium text-gray-900 dark:text-white md:font-light">
 												{item.type}
 											</h2>
 										</div>
-										<ul className="ml-4 mt-2 text-gray-700 dark:text-gray-300">
+										<ul className="mt-2 text-gray-700 dark:text-gray-300 lg:ml-4 ">
 											{item.skills.map((skill) => {
 												return (
 													<li
@@ -46,7 +48,7 @@ const About = () => {
 														id="icon"
 													>
 														{skill.icon}
-														<label htmlFor="icon" className="ml-2">
+														<label htmlFor="icon" className="ml-2 md:text-sm">
 															{skill.name}
 														</label>
 													</li>
