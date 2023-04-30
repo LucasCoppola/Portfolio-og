8e1435d89bd4d35ee501dad50c461a55e037d3f8
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-04-30 13:36:19 -0300

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Landing from './components/Landing'
 import Contact from './components/Contact'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
+import Footer from './components/Footer'
 
 const App = () => {
 	return (
@@ -13,6 +14,7 @@ const App = () => {
 			<About />
 			<Projects />
 			<Contact />
+			<Footer />
 		</main>
 	)
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+
+function Footer() {
+	return (
+		<footer className="bg-gray-900 py-8">
+			<div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+				<div className="flex justify-center space-x-6">
+					<a
+						href="https://github.com/LucasCoppola"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="text-gray-400 transition-colors duration-300 hover:text-white"
+					>
+						<span className="sr-only">GitHub</span>
+						<FaGithub className="h-6 w-6" />
+					</a>
+					<a
+						href="https://linkedin.com/in/lucas-coppola-1805ab226"
+						target="_blank"
+						rel="noopener noreferrer"
+						className="text-gray-400 transition-colors duration-300 hover:text-white"
+					>
+						<span className="sr-only">LinkedIn</span>
+						<FaLinkedin className="h-6 w-6" />
+					</a>
+				</div>
+				<p className="mt-8 text-center text-sm text-gray-400">
+					Designed &amp; built by <span className="text-green-300">Lucas Coppola</span> © 2023
+				</p>
+			</div>
+		</footer>
+	)
+}
+
+export default Footer
