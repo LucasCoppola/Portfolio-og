b8a33578b4ed338fa2689d723eab2a936921b79a
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-05-02 12:17:13 -0300

    Add smooth transition to navbar hide/show effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,21 +14,38 @@ const Navbar = () => {
 			setVisible(prevScrollpos > currentScrollPos || currentScrollPos < 10)
 			setPrevScrollpos(currentScrollPos)
 		}
+
 		if (menuOpen && !visible) {
 			setMenuOpen(false)
+		}
+
+		const navbar = document.querySelector('.navbar')
+		if (navbar) {
+			if (visible) {
+				navbar.classList.remove('navbar--hidden')
+			} else {
+				navbar.classList.add('navbar--hidden')
+			}
 		}
 
 		window.addEventListener('scroll', handleScroll)
 
 		return () => window.removeEventListener('scroll', handleScroll)
-	}, [prevScrollpos])
+	}, [prevScrollpos, menuOpen, visible])
 
 	return (
-		<header className={` top-0 z-10 mb-6 bg-gray-800 ${visible ? 'sticky ' : ''} `}>
-			<div className="flexflex-col container mx-auto flex-wrap items-center p-2 md:flex-row md:justify-between lg:px-20">
+		<header className={`navbar z-10  bg-gray-800 ${visible ? 'sticky ' : ''} `}>
+			<div className="container mx-auto flex flex-col flex-wrap items-center p-2 md:flex-row md:justify-between lg:px-16">
 				<div className="flex w-full items-center justify-between ">
 					<div className="flex-shrink flex-grow">
-						<Link to="landing" className="inline-block" spy={true} smooth={true} offset={50} duration={500}>
+						<Link
+							to="landing"
+							className="inline-block cursor-pointer"
+							spy={true}
+							smooth={true}
+							offset={50}
+							duration={500}
+						>
 							<img
 								src="../../LC.png"
 								alt="logo"
@@ -87,7 +104,7 @@ const Navbar = () => {
 				<nav
 					className={`${
 						menuOpen ? ' flex flex-col md:flex-row ' : 'hidden'
-					} absolute right-[1px] w-full bg-gray-800 px-6 pb-4 md:flex md:items-center md:justify-center`}
+					} absolute right-[1px] mt-16 w-full bg-gray-800 px-6 pb-4 md:flex md:items-center md:justify-center`}
 				>
 					<Link
 						to="about"
@@ -135,36 +152,65 @@ export default Navbar
 /* Back Up Code */
 
 {
-	/* <header className="top-0 z-10 mb-6 bg-gray-800 md:sticky">
-			<div className="container mx-auto flex flex-col flex-wrap items-center p-2 md:flex-row md:justify-between lg:px-20">
+	/* <header className={` top-0 z-10 mb-6 bg-gray-800 ${visible ? 'sticky ' : ''} `}>
+			<div className="container mx-auto flex flex-col flex-wrap items-center p-2 md:flex-row md:justify-between lg:px-16">
 				<div className="flex w-full items-center justify-between ">
 					<div className="flex-shrink flex-grow">
-						<a href="#about">
+						<Link
+							to="landing"
+							className="inline-block cursor-pointer"
+							spy={true}
+							smooth={true}
+							offset={50}
+							duration={500}
+						>
 							<img
 								src="../../LC.png"
 								alt="logo"
-								className="border-white-500 my-1 h-12 w-12 rounded-full border-2 md:mr-auto"
+								className="border-white-500 my-1 h-12 w-12 rounded-full border-2 "
 							/>
-						</a>
+						</Link>
 					</div>
 
-					<div className="ml-auto hidden md:flex">
-						<a href="#projects" className="mt-4 block text-lg text-white hover:text-green-300 md:mt-0">
-							Home
-						</a>
-						<a href="#about" className="ml-6 mt-4 block text-lg text-white hover:text-green-300 md:mt-0">
+					<div className="ml-auto hidden items-center md:flex">
+						<Link
+							to="about"
+							className="ml-6 mt-4 block cursor-pointer text-lg transition duration-200 ease-in-out hover:border-b-2 hover:border-green-300 hover:bg-gray-800 hover:text-white md:mt-0"
+							spy={true}
+							smooth={true}
+							offset={50}
+							duration={500}
+						>
 							About
-						</a>
-						<a href="#skills" className="ml-6 mt-4 block text-lg text-white hover:text-green-300 md:mt-0">
+						</Link>
+						<Link
+							to="projects"
+							className="ml-6 mt-4 block cursor-pointer text-lg transition duration-200 ease-in-out hover:border-b-2 hover:border-green-300 hover:bg-gray-800 hover:text-white md:mt-0"
+							spy={true}
+							smooth={true}
+							offset={50}
+							duration={500}
+						>
 							Projects
-						</a>
-						<a href="#contact" className="ml-6 mt-4 block text-lg text-white hover:text-green-300 md:mt-0">
+						</Link>
+						<Link
+							to="contact"
+							className="ml-6 mt-4 block cursor-pointer text-lg transition duration-200 ease-in-out hover:border-b-2 hover:border-green-300 hover:bg-gray-800 hover:text-white md:mt-0"
+							spy={true}
+							smooth={true}
+							offset={50}
+							duration={500}
+						>
 							Contact
-						</a>
+						</Link>
+						<button className="ml-6 mt-4 block rounded border border-green-300 bg-transparent px-4 py-2 font-semibold text-green-300 transition duration-200 ease-in-out hover:-translate-y-0.5 hover:border-b-4 md:mt-0">
+							Resume
+						</button>
 					</div>
+
 					<div className="flex-shrink flex-grow-0">
 						<button
-							className="ml-auto inline-block rounded px-3 py-2 text-gray-200 hover:text-white focus:outline-none md:hidden"
+							className="ml-auto inline-block rounded px-4 py-2 text-gray-200 hover:text-white focus:outline-none md:hidden"
 							onClick={() => setMenuOpen(!menuOpen)}
 						>
 							<RxHamburgerMenu className={`${menuOpen ? 'hidden' : 'block'} h-8 w-8  `} />
@@ -173,24 +219,47 @@ export default Navbar
 					</div>
 				</div>
 
-				<nav className={`${menuOpen ? ' ml-auto flex flex-col md:flex-row ' : 'hidden'} pl-4 pr-4`}>
-					<a href="#projects" className="md: mt-4 block text-right  text-lg hover:text-white md:mt-0">
-						Home
-					</a>
-
-					<a href="#about" className="md: mt-4 block text-right  text-lg hover:text-white md:mt-0">
+				<nav
+					className={`${
+						menuOpen ? ' flex flex-col md:flex-row ' : 'hidden'
+					} absolute right-[1px] mt-16 w-full bg-gray-800 px-6 pb-4 md:flex md:items-center md:justify-center`}
+				>
+					<Link
+						to="about"
+						className="mt-4 block text-right  text-lg hover:text-white "
+						spy={true}
+						smooth={true}
+						offset={50}
+						duration={500}
+					>
 						About
-					</a>
-
-					<a href="#skills" className="md: mt-4 block text-right  text-lg hover:text-white md:mt-0">
+					</Link>
+					<Link
+						to="projects"
+						className="mt-4 block text-right  text-lg hover:text-white "
+						spy={true}
+						smooth={true}
+						offset={50}
+						duration={500}
+					>
 						Projects
-					</a>
-
-					<a href="#contact" className="mt-4 block  text-right   text-lg hover:text-white md:mt-0">
+					</Link>
+					<Link
+						to="contact"
+						className="mt-4 block text-right  text-lg hover:text-white "
+						spy={true}
+						smooth={true}
+						offset={50}
+						duration={500}
+					>
 						Contact
-					</a>
+					</Link>
+					<button className="ml-auto mt-4 block w-24 rounded border border-green-300 bg-transparent px-4 py-2 text-right font-semibold text-green-300 md:mt-0">
+						<a href="path/to/your/resume.pdf" target="_blank">
+							Resume
+						</a>
+					</button>
 				</nav>
 			</div>
-		</header>
- */
+		</header> */
 }
